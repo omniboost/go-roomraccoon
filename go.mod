2a6e903c2f138c7c3a36363a6615211a1dbb2d3d
Bart Versluijs <bartversluijs@icloud.com>
2024-06-12 16:31:37 +0200

    feat: added support for `/venues/{venue_id}/bookings`

--- go.mod
+++ go.mod
@@ -3,8 +3,7 @@ module github.com/omniboost/go-venuesuite
 go 1.22
 
 require (
-	github.com/cydev/zero v0.0.0-20160322155811-4a4535dd56e7
-	github.com/gorilla/schema v0.0.0-20171211162101-9fa3b6af65dc
+	github.com/gorilla/schema v1.3.0
 	github.com/pkg/errors v0.9.1
 	gopkg.in/guregu/null.v3 v3.5.0
 )
---
+++ venue_bookings_get_test.go
@@ -0,0 +1,53 @@
+package venuesuite_test
+
+import (
+	"encoding/json"
+	"fmt"
+	"os"
+	"strconv"
+	"testing"
+	"time"
+
+	"github.com/omniboost/go-venuesuite"
+)
+
+func TestVenueBookingsGet(t *testing.T) {
+	req := client.NewVenueBookingsGet()
+	venueID, err := strconv.Atoi(os.Getenv("VENUE_ID"))
+	if err != nil {
+		t.Error(err)
+	}
+
+	req.PathParams().VenueID = venueID
+	req.QueryParams().Start = venuesuite.Date{time.Date(
+		2024, 1, 1, 0, 0, 0, 0, time.UTC,
+	)}
+	req.QueryParams().End = venuesuite.Date{time.Date(
+		2024, 5, 1, 0, 0, 0, 0, time.UTC,
+	)}
+	req.QueryParams().Status = []string{"draft", "booked"}
+	resp, err := req.Do()
+	if err != nil {
+		t.Error(err)
+	}
+
+	b, _ := json.MarshalIndent(resp, "", "  ")
+	fmt.Println(string(b))
+}
+
+func TestVenueBookingsGetAll(t *testing.T) {
+	req := client.NewVenueBookingsGet()
+	venueID, err := strconv.Atoi(os.Getenv("VENUE_ID"))
+	if err != nil {
+		t.Error(err)
+	}
+
+	req.PathParams().VenueID = venueID
+	resp, err := req.All()
+	if err != nil {
+		t.Error(err)
+	}
+
+	b, _ := json.MarshalIndent(resp, "", "  ")
+	fmt.Println(string(b))
+}
